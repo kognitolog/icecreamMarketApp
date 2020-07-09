b79a865ff77eb48f7e026fb28060c0380a552915
Max Kozlov <max060290@gmail.com>
2020-07-09 11:02:53 +0300

    Initial commit

---
+++ src/main/java/develop/kozlov/IcecreamMarketApplication.java
@@ -0,0 +1,13 @@
+package develop.kozlov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcecreamMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcecreamMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/develop/kozlov/IcecreamMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package develop.kozlov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcecreamMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
